0c16d01c195448f7fd68987a247f896d75a8232c
MU94W <nanqiao15@126.com>
2017-12-28 01:31:00 +0800

    bug fix

--- tests/create_wav_mel_stftm_tfrecords_test.py
+++ tests/create_wav_mel_stftm_tfrecords_test.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.call(["python", "../create_wav_mel_stftm_tfrecords.py", "-s", "./audios"])
+subprocess.call(["python", "../create_wav_mel_stftm_tfrecords.py", "-s", "./audios", "--sr", "48000"])
